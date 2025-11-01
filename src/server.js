a262457f1044390ed2fadaaef9e1f0ea0712918f
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-11-01 02:10:55 +0100

    Add server app for branch

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import PinoHttp from 'pino-http';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import PinoHttp from "pino-http";
 
 dotenv.config();
 
@@ -14,24 +14,24 @@ app.use(PinoHttp());
 
 
 
-app.get('/notes', (req, res) => {
-res.status(200).json({ message: 'Retrieved all notes' });
+app.get("/notes", (req, res) => {
+res.status(200).json({ message: "Retrieved all notes" });
 });
 
-app.get('/notes/:noteid', (req, res) => {
-   const { noteid } = req.params;
-   res.status(200).json({ message: `Retrieved note with ID: ${noteid}` });
+app.get("/notes/:noteId", (req, res) => {
+   const { noteId } = req.params;
+   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
-app.get('/test-error', (req, res) => {
-    throw new Error('Simulated server error');
+app.get("/test-error", () => {
+    throw new Error("Simulated server error");
 });
 
 app.use((req, res) => {
-    res.status(404).json({ message: 'Route Not Found' });
+    res.status(404).json({ message: "Route Not Found" });
 } );
-app.use((err, req, res, next) => {
-    console.error('server error:', err.message);
+app.use((err, req, res) => {
+    console.error("server error:", err.message);
     res.status(500).json({ message: err.message  });
 });
 
