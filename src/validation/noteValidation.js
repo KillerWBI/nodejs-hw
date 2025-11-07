ea05906f1b0ec4907dabc9de70c77bc703d2152a
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-11-07 18:05:28 +0100

    finish 2 dz

---
+++ src/validation/noteValidation.js
@@ -0,0 +1,47 @@
+import { Joi, Segments } from "celebrate";
+import { isValidObjectId } from "mongoose";
+import tags from "../contacts/tags.js"; // перелік тегів із завдання
+
+// ✅ Кастомна перевірка для ObjectId
+const objectIdValidator = (value, helpers) => {
+  if (!isValidObjectId(value)) {
+    return helpers.error("any.invalid");
+  }
+  return value;
+};
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...tags).optional(),
+    search: Joi.string().allow("").optional(),
+  }),
+};
+
+export const noteIdSchema = {
+ [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+ })
+
+};
+
+export const createNoteSchema = {
+[ Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string(),
+    tag: Joi.string().valid (...tags).optional(),
+})
+
+};
+
+export const updateNoteSchema = {
+    [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+     }),
+    [ Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).optional(),
+    content: Joi.string().optional(),
+    tag: Joi.string().valid(...tags).optional(),
+  }).or("title", "content", "tag"),
+};
