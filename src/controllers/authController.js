694cc726759bf7809d895009e4049a2f703a0063
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-11-11 03:07:36 +0100

    finish 6/10

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,9 @@
 import bcrypt from "bcryptjs";
 import creatHttpError from "http-errors";
+import { Session } from "../models/session.js";
 import { User } from "../models/user.js";
 import { createSession, setSessionCookies } from "../services/auth.js";
+import { createHttpError } from "../utils/errors.js";
 
 export const registerUser = async (req, res, next) => {
 
@@ -24,3 +26,59 @@ export const registerUser = async (req, res, next) => {
 
     res.status(201).json({newUser});
 };
+
+export const loginUser = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email});
+    if (!user) {
+        return next(creatHttpError(400, "Email in use"));
+    }
+    const isValidPassword = await bcrypt.compare(password, User.password);
+    if (!isValidPassword) {
+        return next(creatHttpError(401, "Email or password is wrong"));
+    }
+
+    await Session.deleteOne({ userId: user._id});
+
+    const newSession = await createSession(user._id);
+    setSessionCookies(res, newSession);
+
+
+    res.status(200).json(user);
+};
+
+export const refreshUserSession = async (req, res, next) => {
+
+    const { sessionId, refreshToken } = req.cookies;
+
+    if (!sessionId || !refreshToken) {
+      return next(createHttpError(401, "Session not found"));
+    }
+
+    const session = await Session.findOne({
+      _id: sessionId,
+      refreshToken,
+    });
+
+    if (!session) {
+      return next(createHttpError(401, "Session not found"));
+    }
+
+    const isExpired = session.refreshTokenValidUntil < new Date();
+
+    if (isExpired) {
+      return next(createHttpError(401, "Session token expired"));
+    }
+
+    await Session.deleteOne({ _id: sessionId });
+
+    const newSession = await createSession(session.userId);
+
+    setSessionCookies(res, newSession);
+
+    res.status(200).json({
+      message: "Session refreshed",
+    });
+
+  };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,14 @@
 import { celebrate } from "celebrate";
 import { Router } from "express";
-import { registerUser } from "../controllers/authController.js";
-import { registerUserSchema } from "../validations/authValidation.js";
+import { loginUser, refreshUserSession, registerUser } from "../controllers/authController.js";
+import { loginUserShema, registerUserSchema } from "../validations/authValidation.js";
+
 
 const router = Router();
 
 router.post("/auth/register", celebrate(registerUserSchema), registerUser);
+router.post("auth/login", celebrate(loginUserShema), loginUser);
+router.post("auth/refresh", refreshUserSession);
 
 
 export default router;
