20879b01d094a9102e427a393d554e371324f5c4
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-11-11 19:13:10 +0100

    finish 9/10

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,10 +1,12 @@
 import { celebrate } from "celebrate";
 import { Router } from "express";
 import { createNote, deleteNote, getAllNotes, getNoteById, updateNote } from "../controllers/notesController.js";
+import authenticate from "../middleware/authenticate.js";
 import { createNoteSchema, getAllNotesSchema, noteIdSchema, updateNoteSchema } from "../validations/notesValidation.js";
-
 const router = Router();
 
+
+router.use(authenticate);
 router.get("/notes", celebrate(getAllNotesSchema) ,getAllNotes);
 router.get("/notes/:noteId", celebrate(noteIdSchema),getNoteById);
 router.post("/notes", celebrate(createNoteSchema),createNote);
