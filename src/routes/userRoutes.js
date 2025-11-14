0a13e8ac833295e28d7c278885795c7d86714749
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-11-15 00:45:58 +0100

    2/2

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { updateUserAvatar } from "../controllers/UserController.js";
 import { authenticate } from "../middleware/authenticate.js";
+import { upload } from "../middleware/multer.js";
 
 
 const router = Router();
 
-router.patch("/users/me/avatar",authenticate,updateUserAvatar,);
+router.patch("/users/me/avatar",authenticate,upload.single("avatar"), updateUserAvatar,);
 
 
 
