8f992b9c4177b6671a03f1ac626e4fef4c21d6f5
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-11-15 12:48:15 +0100

    Fix file casing for userController

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { updateUserAvatar } from "../controllers/userController.js";
 import { authenticate } from "../middleware/authenticate.js";
 import { upload } from "../middleware/multer.js";
 
 
 const router = Router();
 
-router.patch("/users/me/avatar",authenticate,upload.single("avatar"), );
+router.patch("/users/me/avatar",authenticate,upload.single("avatar"), updateUserAvatar);
 
 
 
